5a401e9893d122ee0aa2ce5be7bb64066d51adc2
Joshua Vayer <joshua.vayer@gmail.com>
2022-10-18 18:08:40 +0200

    Misc: init test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import QrReader from "react-qr-scanner";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QrReader
+        delay={300}
+        onError={console.error}
+        onScan={param => console.log(param)}
+        constraints={{ video: { facingMode: "environment" } }}
+        style={{ width: "100%" }}
+      />
     </div>
   );
 }
